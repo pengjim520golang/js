70a98c3724ef0585c3bae1cad30947149c107274
pengjim520golang <84207336@qq.com>
2019-01-14 17:03:48 +0800

    javascript基础

---
+++ javascript基础/代码/火链区块链学院1期/第3天-2019-01-14/coin.js
@@ -0,0 +1,7 @@
+var coin = {
+    name:"BTC",
+    address:"0x123434532452345",
+    priKey:"0x234gsdfgsdfgsdfgsdfg",
+    pubKey:"sfasdfasdfalksdfjasldfjasldfjalsdjfalsdjflasdjf",
+    byteCode:"34345823045823405823045230945230452304852345"
+}
