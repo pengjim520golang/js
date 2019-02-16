eae9d4c6fa8417691a96645140eaab4c75a361ed
pengjim520golang <84207336@qq.com>
2019-02-16 11:36:41 +0800

    js高级

---
+++ javascript高级/代码/闭包代码/js压缩/js_iife.js
@@ -0,0 +1,161 @@
+//这是我定义个关于欢乐信息的打印函数
+;(function(window){
+    var message = "this is a happy message"
+    function Go(){
+        console.log(message)
+    }
+    function Play(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+
+    function display(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+
+	function show(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+
+    window.Go = Go;
+})(window)
+
+
+//这是我定义个关于欢乐信息的打印函数
+;(function(window){
+    var message = "this is a happy message"
+    function Go1(){
+        console.log(message)
+    }
+    function Play2(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+
+    function display3(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+
+	function show4(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+})(window)
+
+//这是我定义个关于欢乐信息的打印函数
+;(function(window){
+    var message = "this is a happy message"
+    function Go7(){
+        console.log(message)
+    }
+    function Play7(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+
+    function display7(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+
+	function show7(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+
+    window.Go7 = Go7;
+})(window)
+
---
+++ javascript高级/代码/闭包代码/js压缩/js_src_mod.js
@@ -0,0 +1,160 @@
+;(function(){
+    var message = "this is a happy message"
+    function Go(){
+        console.log(message)
+    }
+    function Play(){
+      
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+    }
+
+    function display(){
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+    }
+
+	function show(){
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+    }
+
+    window.Go = Go;
+})()
+
+
+
+;(function(){
+    var message = "this is a happy message"
+    function Go1(){
+        console.log(message)
+    }
+    function Play2(){
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+
+        console.log(message)
+    }
+
+    function display3(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+
+	function show4(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+})()
+
+//这是我定义个关于欢乐信息的打印函数
+;(function(){
+    var message = "this is a happy message"
+    function Go7(){
+        console.log(message)
+    }
+    function Play7(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+
+    function display7(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+
+	function show7(){
+        //这是打印第1次
+        console.log(message)
+        //这是打印第2次
+        console.log(message)
+        //这是打印第3次
+        console.log(message)
+        //这是打印第4次
+        console.log(message)
+        //这是打印第5次
+        console.log(message)
+        //这是打印第6次
+        console.log(message)
+    }
+
+    window.Go7 = Go7;
+})()
+
